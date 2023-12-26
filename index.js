521fe712cdd9fb3f7ee9b12e83c176900a0d85c3
manishkaswan <manishkaswan5@gmail.com>
2023-12-26 10:23:13 +0530

    dltwork

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const btn = document.getElementById("btn");
 const dataContainer = document.getElementById('data-container');
-
+const appointmentbtn = document.createElement('button');
+function createDeleteButton(appointmentId) {
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', () => {
+        deleteAppointment(appointmentId);
+    });
+    return deleteButton;
+}
+function deleteAppointment(appointmentId) {
+    axios.delete(`https://crudcrud.com/api/9613efbd7846417ba3ebdf9cdca74e28/appointmentdata/${appointmentId}`)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
 btn.addEventListener("click", () => {
     const namef = document.getElementById("namef").value;
     const emailf = document.getElementById("emailf").value;
@@ -33,9 +50,17 @@ window.addEventListener('load', () => {
                     <hr>
                 `;
                 dataContainer.appendChild(appointmentDiv);
+                const deleteButton = createDeleteButton(appointment._id);
+                appointmentDiv.appendChild(deleteButton);
+                dataContainer.appendChild(appointmentDiv);
+                appointmentbtn.addEventListener("click",()=>{
+                    console.log("i'm delete button");
+                    // axios.delete("https://crudcrud.com/api/9613efbd7846417ba3ebdf9cdca74e28/appointmentdata")
+                })
             });
         })
         .catch((error) => {
             console.log(error);
         });
+        
 });
